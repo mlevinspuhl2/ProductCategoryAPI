07447593506140c5aae441a37b2f8b872e4dac7a
Marcel Levinspuhl <marcel.levinspuhl@weareplanet.com>
2023-11-13 09:50:57 +0000

    test started

--- TestProductCategory/CategoryServiceTest.cs
+++ TestProductCategory/CategoryServiceTest.cs
@@ -1,11 +1,60 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using ProductCategoryAPI.models;
+using ProductCategoryAPI.Services;
+using System.Reflection;
+using Xunit;
+
 namespace TestProductCategory
 {
     public class CategoryServiceTest
     {
-        [Fact]
-        public void Test1()
+        private IHostBuilder GetWorkerService()
         {
+            return Host.CreateDefaultBuilder()
+                        .ConfigureAppConfiguration((hostContext, config) =>
+                        {
+                            config.AddJsonFile("appsettings_test.json", false, false);
+                        })
+                        .ConfigureServices((hostContext, services) =>
+                        {
+                            services.AddAutoMapper(typeof(Program));
+                            services.AddScoped<ICategoryService, CategoryService>();
+                            services.AddScoped<IProductService, ProductService>();
+                            services.Configure<MongoDBSettings>(hostContext
+                                .Configuration.GetSection(nameof(MongoDBSettings)));
+                            services.AddSingleton<MongoDBContext>(serviceProvider =>
+                            {
+                                var settings = serviceProvider.GetRequiredService<IOptions<MongoDBSettings>>().Value;
+                                return new MongoDBContext(settings.ConnectionString, settings.DatabaseName);
+                            });
+                        });
+                    
 
+        }
+        [Fact]
+        public async void Get_Test()
+        {
+            // Arrange
+            var service = GetWorkerService().Build().Services.GetService <ICategoryService>();
+
+ 
+            session.StartTransaction(new TransactionOptions(
+                                         readConcern: ReadConcern.Snapshot,
+                                         writeConcern: WriteConcern.WMajority));
+
+
+
+            // Act
+            var categories = await service.Get();
+
+            // Assert
+            Assert.NotNull(categories);
         }
     }
 }
